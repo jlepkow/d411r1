93a0f093e75ffeb991608bf9a8d0c4fe2d8b5ea5
jlepkow <jlepkow@wgu.edu>
2025-01-26 17:31:12 -0600

    Update to Restore-SQL.ps1

--- Restore-SQL.ps1
+++ Restore-SQL.ps1
@@ -7,57 +7,70 @@ try {
     $sqlServerInstanceName = ".\SQLEXPRESS"
     $databaseName = "ClientDB"
     
-    # Check for database existence
-    $databaseExists = Invoke-Sqlcmd -ServerInstance $sqlServerInstanceName -Query "SELECT COUNT(*) FROM sys.databases WHERE name = '$databaseName'"
-
-    if ($databaseExists -eq 1) {
-        Write-Host -ForegroundColor Yellow "Database '$databaseName' exists. Deleting..."
-        Invoke-Sqlcmd -ServerInstance $sqlServerInstanceName -Query "DROP DATABASE [$databaseName]"
-        Write-Host -ForegroundColor Green "Database '$databaseName' has been deleted."
+    # Check for database existence and output message
+    $query = "SELECT COUNT(*) as Count FROM sys.databases WHERE name = '$databaseName'"
+    $result = Invoke-Sqlcmd -ServerInstance $sqlServerInstanceName -Query $query
+    
+    if ($result.Count -gt 0) {
+        Write-Host "Database '$databaseName' exists."
+        Write-Host "Deleting existing database..."
+        Invoke-Sqlcmd -ServerInstance $sqlServerInstanceName -Query "ALTER DATABASE [$databaseName] SET SINGLE_USER WITH ROLLBACK IMMEDIATE; DROP DATABASE [$databaseName]"
+        Write-Host "Database '$databaseName' has been deleted."
     } else {
-        Write-Host -ForegroundColor Green "Database '$databaseName' does not exist."
+        Write-Host "Database '$databaseName' does not exist."
     }
 
-    # Create the new database
-    Write-Host "Creating database '$databaseName'..."
+    # Create new database
+    Write-Host "Creating new database '$databaseName'..."
     Invoke-Sqlcmd -ServerInstance $sqlServerInstanceName -Query "CREATE DATABASE [$databaseName]"
-    Write-Host -ForegroundColor Green "Database '$databaseName' has been created."
+    Write-Host "Database '$databaseName' has been created."
 
-    # Create the new table
-    $tableName = "Client_A_Contacts"
-    Write-Host "Creating table '$tableName'..."
-    $tableScript = @"
-CREATE TABLE [$databaseName].[dbo].[$tableName] (
-    ContactID INT PRIMARY KEY,
-    FirstName NVARCHAR(50),
-    LastName NVARCHAR(50),
-    Email NVARCHAR(100),
-    Phone NVARCHAR(15)
-)
+    # Create table with auto-incrementing ID
+    Write-Host "Creating table 'Client_A_Contacts'..."
+    $createTableQuery = @"
+    CREATE TABLE [Client_A_Contacts] (
+        ID INT IDENTITY(1,1) PRIMARY KEY,
+        FirstName NVARCHAR(50),
+        LastName NVARCHAR(50),
+        City NVARCHAR(50),
+        County NVARCHAR(50),
+        Zip NVARCHAR(10),
+        OfficePhone NVARCHAR(20),
+        MobilePhone NVARCHAR(20)
+    )
 "@
-    Invoke-Sqlcmd -ServerInstance $sqlServerInstanceName -Database $databaseName -Query $tableScript
-    Write-Host -ForegroundColor Green "Table '$tableName' has been created."
+    Invoke-Sqlcmd -ServerInstance $sqlServerInstanceName -Database $databaseName -Query $createTableQuery
+    Write-Host "Table 'Client_A_Contacts' has been created."
 
     # Import data from CSV
-    $csvPath = ".\NewClientData.csv"
-$csvData = Import-Csv $csvPath
+    Write-Host "Importing data from NewClientData.csv..."
+    $csvPath = Join-Path $PSScriptRoot "NewClientData.csv"
+    $csvData = Import-Csv $csvPath
 
-foreach ($row in $csvData) {
-    $insertQuery = @"
-INSERT INTO [$tableName] (ContactID, FirstName, LastName, Email, Phone)
-VALUES ('$($row.ContactID)', '$($row.FirstName)', '$($row.LastName)', '$($row.Email)', '$($row.Phone)')
+    foreach ($row in $csvData) {
+        $insertQuery = @"
+        INSERT INTO Client_A_Contacts (FirstName, LastName, City, County, Zip, OfficePhone, MobilePhone)
+        VALUES (
+            '$(($row.first_name -replace "'", "''"))',
+            '$(($row.last_name -replace "'", "''"))',
+            '$(($row.city -replace "'", "''"))',
+            '$(($row.county -replace "'", "''"))',
+            '$(($row.zip -replace "'", "''"))',
+            '$(($row.officePhone -replace "'", "''"))',
+            '$(($row.mobilePhone -replace "'", "''"))'
+        )
 "@
-    Invoke-Sqlcmd -ServerInstance $sqlServerInstanceName -Database $databaseName -Query $insertQuery
-}
+        Invoke-Sqlcmd -ServerInstance $sqlServerInstanceName -Database $databaseName -Query $insertQuery
+    }
+    Write-Host "Data import completed successfully."
 
-Write-Host -ForegroundColor Green "Data imported successfully from $csvPath"
-
-# Generate output file
-Invoke-Sqlcmd -Database ClientDB -ServerInstance .\SQLEXPRESS -Query 'SELECT * FROM dbo.Client_A_Contacts' > .\SqlResults.txt
-Write-Host -ForegroundColor Green "Results exported to SqlResults.txt"
+    # Generate output file
+    Write-Host "Generating SqlResults.txt..."
+    Invoke-Sqlcmd -Database ClientDB -ServerInstance .\SQLEXPRESS -Query 'SELECT * FROM dbo.Client_A_Contacts' > .\SqlResults.txt
+    Write-Host "SqlResults.txt has been generated."
 
 } catch {
-    Write-Host -ForegroundColor Red "An error occurred:"
-    Write-Host -ForegroundColor Red $_.Exception.Message
-    Write-Host -ForegroundColor Red $_.ScriptStackTrace
+    Write-Host "Error occurred: $_"
+    Write-Host "Stack Trace: $($_.ScriptStackTrace)"
+    throw
 }
